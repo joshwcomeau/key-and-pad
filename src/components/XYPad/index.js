0da801b26124ec083f9bc42d5923ad666a262d81
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-22 19:03:11 -0400

    Create generic pad, pass X/Y positions to handlers

--- src/components/XYPad/index.js
+++ src/components/XYPad/index.js
@@ -1,15 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './index.css';
 
 class XYPad extends Component {
+  constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+    this.touch = this.touch.bind(this);
+    this.release = this.release.bind(this);
+  }
+
+  click(ev) {
+    // If, and only if, the mouse button is held down, we want to delegate
+    // to the `touch` event.
+    if (ev.buttons === 1) {
+      this.touch(ev);
+    }
+  }
+
+  touch(ev) {
+    // We need to calculate the relative position of the event, and pass it
+    // onto our supplied touch handler.
+    const boundingBox = this.elem.getBoundingClientRect();
+
+    const x = (ev.clientX - boundingBox.left) / boundingBox.width;
+    const y = (ev.clientY- boundingBox.top) / boundingBox.height;
+
+    this.props.onTouch({ x, y });
+  }
+
+  release(ev) {
+
+  }
+
   render() {
+    const { width, height } = this.props;
+
     return (
-      <div className="x-y-pad">
-        <h2>Hello there!</h2>
-        <p>How are you?</p>
-      </div>
+      <div
+        className="x-y-pad"
+        ref={elem => this.elem = elem}
+        style={{ width, height }}
+        onTouchMove={this.touch}
+        onMouseDown={this.touch}
+        onMouseMove={this.click}
+        onTouchEnd={this.props.onRelease}
+        onMouseUp={this.props.onRelease}
+      />
     );
   }
 }
+
+XYPad.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  onTouch: PropTypes.func.isRequired,
+  onRelease: PropTypes.func.isRequired,
+};
+
+XYPad.defaultProps = {
+  width: 350,
+  height: 350,
+};
 
 export default XYPad;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import XYPad from './components/XYPad';
+
 import './reset.css';
 import './index.css';
 
+injectTapEventPlugin();
+
+
 ReactDOM.render(
-  <XYPad />,
+  <XYPad
+    onTouch={({ x, y }) => console.log(x, y)}
+    onRelease={(ev) => console.log('released!', ev)}
+  />,
   document.getElementById('root')
 );
