a1230531e45b4b7794933b574a484723ee0d935c
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-22 18:40:27 -0400

    Initial commit. Generated project from create-react-app

---
+++ src/components/XYPad/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class XYPad extends Component {
+  render() {
+    return (
+      <div className="x-y-pad">
+        <h2>Hello there!</h2>
+        <p>How are you?</p>
+      </div>
+    );
+  }
+}
+
+export default XYPad;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import XYPad from './components/XYPad';
+import './reset.css';
+import './index.css';
+
+ReactDOM.render(
+  <XYPad />,
+  document.getElementById('root')
+);
