f8a604f4a5d44fad2c0a34cb5c20266d4367275a
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-23 09:33:34 -0400

    Style keys

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -1,9 +1,16 @@
 import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
 import './index.css';
 
-const Key = ({ letter }) => {
+
+const Key = ({ letter, keyStyle, ...data}) => {
+  console.log(letter, keyStyle, data);
+
+  const classes = classNames('key', keyStyle);
+
   return (
-    <div className="key">
+    <div className={classes}>
       {letter}
     </div>
   );
