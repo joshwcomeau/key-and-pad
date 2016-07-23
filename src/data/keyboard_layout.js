7ba0269084d4e6cf01e50668d1eb3b9caf1b98f5
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-23 09:21:53 -0400

    Add keyboard mapping

---
+++ src/data/keyboard_layout.js
@@ -0,0 +1,8 @@
+const keyboardLayout = [
+  [undefined, '2', '3', undefined, '5', '6', '7', undefined, '9', '0'],
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  [undefined, 's', 'd', undefined, 'g', 'h', 'j', undefined, 'l', ';'],
+  ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
+];
+
+export default keyboardLayout
