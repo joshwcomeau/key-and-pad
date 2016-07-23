08f4c7eda8eff3fa5193c26d34a36c6c5e4d48d8
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-23 10:31:08 -0400

    Add Keyboard duck, two basic actions

---
+++ src/ducks/keyboard.duck.js
@@ -0,0 +1,49 @@
+import omit from 'lodash.omit';
+
+const initialState = {
+  keys: {},
+};
+
+
+// ////////////////////////
+// ACTION TYPES //////////
+// //////////////////////
+export const PRESS_KEY = 'KEYBOARD/PRESS_KEY';
+export const RELEASE_KEY = 'KEYBOARD/RELEASE_KEY';
+
+
+// ////////////////////////
+// REDUCERS //////////////
+// //////////////////////
+export default function keyboardReducer(state = initialState, action) {
+  switch (action.type) {
+    case PRESS_KEY: {
+      return {
+        ...state.keys,
+        [action.letter]: action.frequency,
+      };
+    }
+
+    case RELEASE_KEY: {
+      return omit(state, action.key);
+    }
+
+    default:
+      return state;
+  }
+}
+
+
+// ////////////////////////
+// ACTION CREATORS ///////
+// //////////////////////
+export const pressKey = ({ letter, frequency}) => ({
+  type: PRESS_KEY,
+  letter,
+  frequency,
+});
+
+export const releaseKey = letter => ({
+  type: RELEASE_KEY,
+  letter,
+});
