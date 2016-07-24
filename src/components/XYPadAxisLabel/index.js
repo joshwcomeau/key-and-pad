51ab2ec6588ac468669621b856f25eb70c3c7150
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-24 09:26:39 -0400

    Update non-colour styles

--- src/components/XYPadAxisLabel/index.js
+++ src/components/XYPadAxisLabel/index.js
@@ -2,8 +2,7 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
 import './index.scss';
-import iconArrowLeft from '../../images/ic_chevron_left_black_24px.svg';
-import iconArrowRight from '../../images/ic_chevron_right_black_24px.svg';
+
 
 const XYPadAxisLabel = ({ label, className, includeRightArrow, includeLeftArrow }) => (
   <div className={classNames('x-y-pad-axis-label', className)}>
