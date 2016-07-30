ac20bff27712dfb6a1c4778f943788eb106043d9
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-30 12:05:40 -0400

    Create Onboarding duck. Start plotting introduction

---
+++ src/ducks/onboarding.duck.js
@@ -0,0 +1,72 @@
+const initialState = {
+  stage: 'initial'
+};
+
+// STAGES
+// The onboarding flow is linear, and involves the following stages:
+//
+// - initial
+//     No keyboard is shown, only a simple "Why hello there!" message in the center
+//     of the screen.
+//     ENDING TRIGGER: time
+//
+// - keys-introduced
+//     The keyboard fades in, centered, and text below reads "These are your keys"
+//     a downward bouncing arrow encourages the user to press a letter on their physical
+//     keyboard.
+//     ENDING TRIGGER: pressing a key (or two?)
+//
+// - keys-confirmed
+//     The text changes to a confirmation. The bouncing arrow stops. Maybe a
+//     checkmark somewhere?
+//     ENDING TRIGGER: time
+//
+// - pad-introduced
+//     The keyboard slides to the left, and the XY Pad fades in.
+//     The text changes to explain what it is.
+//     ENDING TRIGGER: pressing a key while the pad is depressed
+//
+// - pad-confirmed
+//     The text changes to a confirmation.
+//     ENDING TRIGGER: time
+//
+// - control-panel-introduced
+//     The keyboard and pad slide up, and a control panel fades in, in the center
+//     of the screen. Below that, the text explains that the sounds can be tweaked
+//     by using these controls. Encourage experimentation
+//     ENDING TRIGGER: unsure. Either time, or changing a control.
+//
+// - completed
+//     The keyboard, pad, and control panel all revert to their normal position.
+//     No text is shown.
+//     Update localStorage so that they are not shown the onboarding again.
+
+
+// ////////////////////////
+// ACTION TYPES //////////
+// //////////////////////
+export const UPDATE_STAGE = 'ONBOARDING/UPDATE_STAGE';
+
+
+// ////////////////////////
+// REDUCERS //////////////
+// //////////////////////
+export default function onboardingReducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_STAGE: {
+      return { stage: action.stage };
+    }
+
+    default:
+      return state;
+  }
+}
+
+
+// ////////////////////////
+// ACTION CREATORS ///////
+// //////////////////////
+export const updateStage = ({ stage }) => ({
+  type: UPDATE_STAGE,
+  stage
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import notes from '../ducks/notes.duck';
 import oscillators from '../ducks/oscillators.duck';
 import effects from '../ducks/effects.duck';
+import onboarding from '../ducks/onboarding.duck';
 
 
-export default combineReducers({ notes, oscillators, effects });
+export default combineReducers({ notes, oscillators, effects, onboarding });
