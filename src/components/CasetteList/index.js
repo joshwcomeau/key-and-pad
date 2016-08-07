61c1710173f6d81e42ab0a02ddb6da1a62b553ee
Joshua Comeau <joshwcomeau@gmail.com>
2016-08-07 09:57:10 -0400

    Add selectCasette animation

--- src/components/CasetteList/index.js
+++ src/components/CasetteList/index.js
@@ -1,30 +1,73 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
-import { selectCasette } from '../../ducks/vcr-player.duck';
+import * as actionCreators from '../../ducks/vcr-player.duck';
 import Casette from '../Casette';
 import './index.scss';
 
 
-const CasetteList = ({ casettes, selectCasette }) => {
-  const casetteIds = Object.keys(casettes);
+class CasetteList extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="casette-list">
-      {casetteIds.map(id => (
+    this.animateCasetteSelection = this.animateCasetteSelection.bind(this);
+    this.renderCasette = this.renderCasette.bind(this);
+
+    // While the actual `selectedCasette` value lives in the redux store,
+    // we want to animate the selection process. Therefore, we first set this
+    // component's local state, base the animation off of it, and when it's
+    // complete we dispatch the action to change the actual value.
+    this.state = {
+      selectedCasette: null,
+    };
+  }
+
+  animateCasetteSelection({ id }) {
+    this.setState({
+      selectedCasette: id,
+    });
+
+    window.setTimeout(() => {
+      this.props.selectCasette({ id });
+    }, 1000);
+  }
+
+  renderCasette(id) {
+    const { casettes } = this.props;
+    const { selectedCasette } = this.state;
+
+    const classes = classNames({
+      'casette-wrapper': true,
+      'fading-away': selectedCasette && selectedCasette !== id,
+      'selected': selectedCasette === id,
+    });
+
+    return (
+      <div key={id} className={classes}>
         <Casette
-          key={id}
           sessionName={id}
-          handleClick={selectCasette}
+          handleClick={this.animateCasetteSelection}
           {...casettes[id]}
         />
-      ))}
-    </div>
-  )
+      </div>
+    );
+  }
+
+  render() {
+    const casetteIds = Object.keys(this.props.casettes);
+
+    return (
+      <div className="casette-list">
+        {casetteIds.map(this.renderCasette)}
+      </div>
+    );
+  }
 }
 
 CasetteList.propTypes = {
+  casettes: PropTypes.object,
+  selectCasette: PropTypes.func,
 };
 
 CasetteList.defaultProps = {
@@ -35,5 +78,5 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, {
-  selectCasette,
+  selectCasette: actionCreators.selectCasette,
 })(CasetteList);
