1ddfa717fa0fcb733c1825408aa0034e406c17c8
Joshua Comeau <joshwcomeau@gmail.com>
2016-09-02 08:14:56 -0400

    Prepare production configure-store

--- src/store/configure-store.prod.js
+++ src/store/configure-store.prod.js
@@ -2,8 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createCaptureMiddleware } from 'redux-vcr.capture';
 import { createPersistHandler } from 'redux-vcr.persist';
-import { createRetrieveHandler, createRetrieveMiddleware } from 'redux-vcr.retrieve';
-import { createReplayMiddleware, wrapReducer } from 'redux-vcr.replay';
 
 import rootReducer from '../reducers';
 import onboardingSaga from '../sagas/onboarding.saga';
@@ -14,17 +12,14 @@ export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
 
   const persistHandler = createPersistHandler({ firebaseAuth });
-  const retrieveHandler = createRetrieveHandler({ firebaseAuth });
 
   const middlewares = [
     sagaMiddleware,
     createCaptureMiddleware({ persistHandler }),
-    createRetrieveMiddleware({ retrieveHandler, requiresAuth: false }),
-    createReplayMiddleware({ maximumDelay: 100 }),
   ];
 
   const store = createStore(
-    wrapReducer(rootReducer),
+    rootReducer,
     compose(
       applyMiddleware.apply(this, middlewares)
     )
