8a63f59c4f85340b92af3f70bbb9e769dcaf8f22
Joshua Comeau <joshwcomeau@gmail.com>
2016-09-14 06:38:47 -0400

    Add outdated-browser error message

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -4,13 +4,13 @@ import Icon from '../Icon';
 import './index.scss';
 
 
-const Error = ({ heading, content, linkText, linkHref }) => (
+const Error = ({ heading, content, linkText, linkHref, linkTarget }) => (
   <div className="error">
     <Icon value="error" color="#F35638" size={96} />
     <h3>{heading}</h3>
     <p>{content}</p>
     <p className="error-link">
-      <a href={linkHref}>{linkText}</a>
+      <a href={linkHref} target={linkTarget}>{linkText}</a>
     </p>
   </div>
 );
@@ -20,6 +20,11 @@ Error.propTypes = {
   content: PropTypes.string.isRequired,
   linkText: PropTypes.string,
   linkHref: PropTypes.string,
+  linkTarget: PropTypes.oneOf(['_self', '_blank']),
+};
+
+Error.defaultProps = {
+  linkTarget: '_self',
 };
 
 export default Error;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/App';
 import Error from './components/Error';
 import { initializeWebAudio } from './utils/web-audio-reconciler';
-import { isMobile } from './utils/misc-helpers';
+import { isMobile, isModern } from './utils/misc-helpers';
 import { fadeElementAway } from './utils/dom-helpers';
 import configureStore from './store';
 
@@ -22,7 +22,7 @@ const store = configureStore();
 // and makes any changes required.
 initializeWebAudio(store);
 
-// This is a fairly specialized app: It won't run on mobile or other browsers.
+// Handle unsupported devices and browsers.
 let appComponent;
 if (isMobile()) {
   appComponent = (
@@ -32,6 +32,17 @@ if (isMobile()) {
       content="This web synthesizer needs a mouse and keyboard to operate, and so it cannot run on mobile devices."
       linkText="Here's something neat that will run on your phone :)"
       linkHref="http://martinwecke.de/108/"
+    />
+  );
+} else if (!isModern()) {
+  appComponent = (
+    <Error
+      heading="Sorry, your browser is not supported."
+      // eslint-disable-next-line max-len
+      content="This web synthesizer requires a modern browser to run, since it uses fancy new Web Audio technologies that aren't supported in older browsers."
+      linkText="Please upgrade to a newer browser."
+      linkHref="http://outdatedbrowser.com/en"
+      linkTarget="_blank"
     />
   );
 } else {
